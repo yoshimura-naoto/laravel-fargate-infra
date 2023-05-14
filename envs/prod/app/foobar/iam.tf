058e0b577beccc6c5937985013abd6c199556638
NaotoYoshimura <yoshimura_n@arsaga.jp>
2023-05-14 18:52:35 +0900

    タスク実行ロールにパラメータストア参照権限付与

--- envs/prod/app/foobar/iam.tf
+++ envs/prod/app/foobar/iam.tf
@@ -1,3 +1,4 @@
+# タスク実行ロール
 resource "aws_iam_role" "ecs_task_execution" {
   name = "${local.name_prefix}-${local.service_name}-ecs-task-execution"
 
@@ -25,7 +26,37 @@ data "aws_iam_policy" "ecs_task_execution" {
   arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
 
+# タスク実行権限を付与
 resource "aws_iam_role_policy_attachment" "ecs_task_execution" {
   role = aws_iam_role.ecs_task_execution.name
   policy_arn = data.aws_iam_policy.ecs_task_execution.arn
+}
+
+# パラメータストア参照権限
+resource "aws_iam_policy" "ssm" {
+  name = "${local.name_prefix}-${local.service_name}-ssm"
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "ssm:GetParameters",
+            "ssm:GetParameter"
+          ],
+          "Resource" : "arn:aws:ssm:${data.aws_region.current.id}:${data.aws_caller_identity.self.account_id}:parameter/${local.system_name}/${local.env_name}/*"
+        }
+      ]
+    }
+  )
+
+  tags = {
+    Name = "${local.name_prefix}-${local.service_name}-ssm"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_task_execution_ssm" {
+  role = aws_iam_role.ecs_task_execution.name
+  policy_arn = aws_iam_policy.ssm.arn
 }
