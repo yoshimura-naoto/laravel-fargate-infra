cab116d93d4212488e6314440308b5085df2dac0
NaotoYoshimura <yoshimura_n@arsaga.jp>
2023-05-17 09:54:28 +0900

    ECSデプロイ権限

--- envs/prod/cicd/app_foobar/iam.tf
+++ envs/prod/cicd/app_foobar/iam.tf
@@ -40,3 +40,68 @@ resource "aws_iam_role_policy_attachment" "role_deployer_policy_ecr_power_user" 
   role = aws_iam_role.deployer.name
   policy_arn = data.aws_iam_policy.ecr_power_user.arn
 }
+
+# S3のtfstateを参照する権限
+resource "aws_iam_role_policy" "s3" {
+  name = "s3"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Effect" : "Allow",
+          "Action" : [
+            "s3:GetObject"
+          ],
+          "Resource" : "arn:aws:s3:::naotoyoshimura-tfstate/${local.system_name}/${local.env_name}/cicd/app_${local.service_name}_*.tfstate"
+        }
+      ]
+    }
+  )
+}
+
+# ECSデプロイ権限
+resource "aws_iam_role_policy" "ecs" {
+  name = "ecs"
+  role = aws_iam_role.deployer.id
+
+  policy = jsonencode(
+    {
+      "Version" : "2012-10-17",
+      "Statement" : [
+        {
+          "Sid" : "RegisterTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:RegisterTaskDefinition"
+          ],
+          "Resource" : "*"
+        },
+        {
+          "Sid" : "PassRolesInTaskDefinition",
+          "Effect" : "Allow",
+          "Action" : [
+            "iam:PassRole"
+          ],
+          "Resource" : [
+            data.aws_iam_role.ecs_task.arn,
+            data.aws_iam_role.ecs_task_execution.arn,
+          ]
+        },
+        {
+          "Sid" : "DeployService",
+          "Effect" : "Allow",
+          "Action" : [
+            "ecs:UpdateService",
+            "ecs:DescribeServices"
+          ],
+          "Resource" : [
+            data.aws_ecs_service.this.arn
+          ]
+        }
+      ]
+    }
+  )
+}
